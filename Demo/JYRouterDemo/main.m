26a1cdd19a5a28c495022ea871109b5c2db5a62b
Job-Yang <578093143@qq.com>
2016-12-12 18:14:19 +0800

    init

---
+++ Demo/JYRouterDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JYRouterDemo
+//
+//  Created by 杨权 on 2016/12/12.
+//  Copyright © 2016年 Job-Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
